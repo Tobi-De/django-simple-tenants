224a0e1e4ab3a71694f79fad9d378107006b0c0e
Tobi DEGNON <tobidegnon@proton.me>
2023-01-27 12:28:28 +0100

    feat: add base classes for tenant aware user model

--- simple_tenants/models.py
+++ simple_tenants/models.py
@@ -1,9 +1,12 @@
+from django.contrib.auth.models import UserManager, AbstractUser
 from django.db import models
 from django.utils.text import slugify
 
 from .conf import conf
+from .errors import TenantNotSetError
 from .managers import TenantAwareManager
-from .utils import get_current_tenant
+from .utils import get_current_tenant, tenant_context_disabled
+
 
 # todo: excludes some url
 # and easy way to create first tenant
@@ -40,3 +43,30 @@ class TenantAwareModel(models.Model):
             self.tenant = get_current_tenant()
 
         super().save(*args, **kwargs)
+
+
+class TenantAwareUserManager(UserManager, TenantAwareManager):
+    pass
+
+
+class TenantAwareAbstractUser(AbstractUser):
+    tenant = models.ForeignKey(
+        conf.TENANT_MODEL, on_delete=models.CASCADE, editable=False, null=True
+    )
+
+    objects = TenantAwareUserManager()
+    unscoped = models.Manager()
+
+    class Meta:
+        abstract = True
+
+    def save(self, *args, **kwargs):
+        try:
+            if self._state.adding:
+                self.tenant = get_current_tenant()
+        except TenantNotSetError:
+            if self.is_superuser:
+                with tenant_context_disabled():
+                    super().save(*args, **kwargs)
+            else:
+                super().save(*args, **kwargs)
